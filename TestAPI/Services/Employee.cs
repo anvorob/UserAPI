e8ef5077d1e563506cbb062211c746844733a325
Anatolii Vorobiov <anatolii.vorobiov@gmail.com>
2020-02-27 23:32:28 +1300

    Added delete method
    TODO: add insert query with parameters

--- TestAPI/Services/Employee.cs
+++ TestAPI/Services/Employee.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using TestAPI.Models;
 using Microsoft.Data.Sqlite;
 using System.Data;
+//using System.Data.SQLite;
+//using System.Data.SqlClient;
 
 namespace TestAPI.Services
 {
@@ -39,21 +41,35 @@ namespace TestAPI.Services
             System.Guid guid = System.Guid.NewGuid();
 
             insertCmd.CommandText = "insert into Employee (GUID ,dtcreated ,dtmodified ,dtdeleted , firstname ,lastname ,login ,password ,department ,role ,type ) "+
-                "values('@guid', '@dtcreated','@dtmodified','@dtdeleted','@firstname','@lastname','@login','@password','@department','@role','@type')";
-            insertCmd.Parameters.AddWithValue("@guid", guid.ToString());
-            insertCmd.Parameters.AddWithValue("@dtcreated", DateTime.Now);
-            insertCmd.Parameters.AddWithValue("@dtmodified", DateTime.Now);
-            insertCmd.Parameters.AddWithValue("@dtdeleted", DateTime.MaxValue);
-            insertCmd.Parameters.AddWithValue("@firstname", empl.FirstName);
-            insertCmd.Parameters.AddWithValue("@lastname", empl.LastName);
-            insertCmd.Parameters.AddWithValue("@login", empl.Login);
-            insertCmd.Parameters.AddWithValue("@password", empl.Password);
-            insertCmd.Parameters.AddWithValue("@department", empl.Department);
-            insertCmd.Parameters.AddWithValue("@role", empl.Role);
-            insertCmd.Parameters.AddWithValue("@type", empl.Type);
+                "values(@guid, @dtcreated,@dtmodified,@dtdeleted,@firstname,@lastname,@login,@password,@department,@role,@type)";
+            insertCmd.CommandType = CommandType.Text;
+            //insertCmd.Parameters.Add(new SqlParameter("@guid", guid.ToString()));
+            //insertCmd.Parameters.Add(new SqlParameter("@dtcreated", DateTime.Now));
+            //insertCmd.Parameters.Add(new SqlParameter("@dtmodified", DateTime.Now));
+            //insertCmd.Parameters.Add(new SqlParameter("@dtdeleted", DateTime.MaxValue));
+            //insertCmd.Parameters.Add(new SqlParameter("@firstname", empl.FirstName));
+            //insertCmd.Parameters.Add(new SqlParameter("@lastname", empl.LastName));
+            //insertCmd.Parameters.Add(new SqlParameter("@login", empl.Login));
+            //insertCmd.Parameters.Add(new SqlParameter("@password", empl.Password));
+            //insertCmd.Parameters.Add(new SqlParameter("@department", empl.Department));
+            //insertCmd.Parameters.Add(new SqlParameter("@role", empl.Role));
+            //insertCmd.Parameters.Add(new SqlParameter("@type", empl.Type));
+
+            insertCmd.Parameters.AddWithValue("guid", guid.ToString());
+            insertCmd.Parameters.AddWithValue("dtcreated", DateTime.Now);
+            insertCmd.Parameters.AddWithValue("dtmodified", DateTime.Now);
+            insertCmd.Parameters.AddWithValue("dtdeleted", DateTime.MaxValue);
+            insertCmd.Parameters.AddWithValue("firstname", empl.FirstName);
+            insertCmd.Parameters.AddWithValue("lastname", empl.LastName);
+            insertCmd.Parameters.AddWithValue("login", empl.Login);
+            insertCmd.Parameters.AddWithValue("password", empl.Password);
+            insertCmd.Parameters.AddWithValue("department", empl.Department);
+            insertCmd.Parameters.AddWithValue("role", empl.Role);
+            insertCmd.Parameters.AddWithValue("type", empl.Type);
+
             //insertCmd.Parameters.AddWithValue("$lastloggedin", empl.);
             //insertCmd.Parameters.AddWithValue("$loggedout", empl.FirstName);
-            
+
             insertCmd.ExecuteNonQuery();
 
             connection.sqlite_conn.Close();
@@ -62,7 +78,15 @@ namespace TestAPI.Services
 
         public bool DeleteEmployee(string OID)
         {
-            throw new NotImplementedException();
+            connection.Open();
+
+            var createTableCmd = connection.CreateCommand();
+            createTableCmd.CommandText = "Delete from Employee where guid='" + OID + "'";
+            createTableCmd.CommandType = CommandType.Text;
+
+            int sqlite_datareader = createTableCmd.ExecuteNonQuery();
+            connection.Close();
+            return sqlite_datareader > 0;
         }
 
         public List<ProductionWorker> GetEmployees(string searchWord, string searchField, int limit, int page)
